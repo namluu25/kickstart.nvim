e727f84aceaaf4f6f2e059fea3c9a117b0f210d1
Nam Luu <namluu253@gmail.com>
2024-05-21 07:06:39 +0700

    feat(tmux-nav): update tmux navigation, always show hidden file neotree

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -20,8 +20,21 @@ return {
           ['\\'] = 'close_window',
         },
       },
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          -- '.git',
+          -- '.DS_Store',
+          -- 'thumbs.db',
+        },
+        never_show = {},
+      },
     },
   },
 }
 
 -- vim: ts=2 sts=2 sw=2 et
+
